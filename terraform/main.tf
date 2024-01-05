fdd8cde4bdacce7d1d2990193c7247b0a5d60c2b
ArchanaSounder <archana@cloud-kinetics.com>
2024-01-05 13:32:55 +0530

    Create sandbox-aft Control Tower accountmail id

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "archana@cloud-kinetics.com"
+    AccountEmail              = "sudheeshkrishna@cloud-kinetics.com"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Management"
     SSOUserEmail              = "archana@cloud-kinetics.com"
