af04613d215f9f5c112e996d08dc7b04fab0e224
ArchanaSounder <archana@cloud-kinetics.com>
2024-02-28 12:30:32 +0530

    Create sandbox-aft Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+/*
 module "sandbox" {
   source = "./modules/aft-account-request"
 
@@ -24,4 +25,33 @@ module "sandbox" {
   }
 
   account_customizations_name = "aft-landing-dev01"
+}
+*/
+
+module "sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = var.account_email       #"praveen.soundararajan@cloud-kinetics.com"
+    AccountName               = var.account_name        #"aft-landing-dev02"
+    ManagedOrganizationalUnit = var.managed_ou          #"Sandbox"
+    SSOUserEmail              = var.sso_user_email     #"archana@cloud-kinetics.com"
+    SSOUserFirstName          = var.sso_user_first_name                       #"Archana"
+    SSOUserLastName           = var.sso_user_last_name                     #"Sounder"
+  }
+
+  account_tags = var.account_tags /*{
+    "test" = "AFT"
+  }*/
+
+  change_management_parameters = {
+    change_requested_by =  var.change_requested_by #learn aft
+    change_reason       =var.change_reason    #"Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = var.custom_fields /* {
+    group = "non-prod"
+  }*/
+
+  account_customizations_name = var.account_customizations_name  #"aft-landing-dev02"
 }
---
+++ terraform/variable.tf
@@ -0,0 +1,35 @@
+variable account_email {
+type = string
+
+}
+variable  account_name {
+type = string
+}
+variable managed_ou  {
+type = string
+}
+variable sso_user_email {
+type = string
+}
+variable  sso_user_first_name{
+type = string
+}
+variable  sso_user_last_name{
+type = string
+}
+variable account_tags {
+type = map
+}
+variable change_requested_by {
+type = string
+}
+variable change_reason {
+type = string
+}
+variable account_customizations_name {
+type = string
+}
+
+variable custom_fields {
+type = map
+}
